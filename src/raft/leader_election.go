afc007bb86d0e69a63c6ac97839e01465bb73e6a
a3y3 <sohamssd@gmail.com>
2022-04-09 20:59:44 -0500

    Set leader's matchIndex which will allow it to count itself in the majority when making commit decisions

--- src/raft/leader_election.go
+++ src/raft/leader_election.go
@@ -25,6 +25,7 @@ func (rf *Raft) leader() {
 		rf.setReceivedHeartBeat(true)
 		rf.logMsg(LEAD, "Sending HBs")
 		logEntries := rf.getLogEntries()
+		rf.setMatchIndexFor(rf.me, len(logEntries)-1)
 		for server_idx := range rf.peers {
 			if server_idx != rf.me {
 				go rf.sendLogEntries(server_idx, currentTerm, logEntries)
