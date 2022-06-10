28408e93ce3b6845bc499f5d9e190a8cdbf6e0a5
a3y3 <sohamssd@gmail.com>
2022-06-09 19:54:37 -0500

    Fix a bug that could lead to apply out of order for a recovering server - When a server recovers, it loads it's snapshot from disk. However, before it sends the loaded value to the apply channel, if it gets an AppendEntries RPC, loads the new logs and commits them, it could lead to a problem since the initial values haven't been sent yet.

--- raft/persistance.go
+++ raft/persistance.go
@@ -67,12 +67,15 @@ func (rf *Raft) readPersist(data []byte) bool {
 			}
 
 			go func() {
+				rf.applyLock.Lock()
 				rf.applyCh <- applyMsg
 				rf.logMsg(PERSIST, "Done sending applyMsg") // !help needed. I have absolutely no idea why this applyMsg needs to be sent in a separate thread. There are no locks held, and the testing framework does no special work when a snapshot is sent on this channel.
 				// Despite this, sending this message on the main thread causes the framework to deadlock. I'm at my wit's end on why this would be the case.
+				rf.lastApplied = rf.getSnapshotIndex()
+				rf.commitIndex = rf.getSnapshotIndex()
+				rf.applyLock.Unlock()
 			}()
-			rf.lastApplied = rf.getSnapshotIndex()
-			rf.commitIndex = rf.getSnapshotIndex()
+
 		}
 	}
 	return true
